ec8ee7582b141a56b86818ee95558e41427ac540
niharika18 <69748870+niharikaa18@users.noreply.github.com>
2022-05-28 19:13:52 +0530

    added put.py

---
+++ put.py
@@ -0,0 +1,9 @@
+import requests
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+response = requests.get(api_url)
+print("old", response.json())
+
+
+todo = {"userId": 1, "title": "Wash car", "completed": True}
+response = requests.put(api_url, json=todo)
+print("new", response.json())
